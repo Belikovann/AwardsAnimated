c8f70ea10f4a0c43e9b08ec55659e2a30bbc9ab6
@Annabel_ <hydrant94@gmail.com>
2023-05-09 13:35:56 +0300

    Initial Commit

---
+++ AwardsAnimated/AwardsAnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardsAnimatedApp.swift
+//  AwardsAnimated
+//
+//  Created by Аня Беликова on 09.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct AwardsAnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
